6857ad67cf71714c1ec8c10f326ad46596f28e45
IagoVinicius12 <115949326+IagoVinicius12@users.noreply.github.com>
2023-11-19 16:45:41 -0300

    Update Menu.jsx

--- react-app/src/Components/Menu.jsx
+++ react-app/src/Components/Menu.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+//reaproveitei este componente
 function Menu(props) {
   const [mostrarInputs, setMostrarInputs] = useState(false);
   const [home, setHome]=useState(false);
